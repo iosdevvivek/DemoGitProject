b489751365d90239a7e752a6f66a6cfe25dd44ae
Vivek <vivek.p@rxprism.com>
2020-09-08 17:21:03 +0530

    Initial Commit

---
+++ DemoGitProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoGitProject
+//
+//  Created by Vivek on 9/8/20.
+//  Copyright © 2020 Vivek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
