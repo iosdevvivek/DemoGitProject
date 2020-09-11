2cb9ee620d91829f83ddbefe1acf3c836d4f2cf7
Vivek <vivek.p@rxprism.com>
2020-09-11 16:24:25 +0530

    4 button button created ...

--- DemoGitProject/ViewController.swift
+++ DemoGitProject/ViewController.swift
@@ -19,9 +19,26 @@ class ViewController: UIViewController {
         
         // this is my first commit...
         
+    
+        
       let myFirstButton = UIButton()
         myFirstButton .setTitle("Firstbutton", for: .normal)
          self.view.addSubview(myFirstButton)
+        
+        let myFirstButton2 = UIButton()
+        myFirstButton2 .setTitle("Firstbutton", for: .normal)
+         self.view.addSubview(myFirstButton2)
+        
+        let myFirstButton3 = UIButton()
+               myFirstButton3 .setTitle("Firstbutton", for: .normal)
+                self.view.addSubview(myFirstButton3)
+        let myFirstButton4 = UIButton()
+                    myFirstButton4 .setTitle("Firstbutton", for: .normal)
+                     self.view.addSubview(myFirstButton4)
+        
+        let myFirstButton5 = UIButton()
+                           myFirstButton4 .setTitle("Firstbutton", for: .normal)
+                            self.view.addSubview(myFirstButton4)
         
     }
 
